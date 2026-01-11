966ce4b15717cfc32e441e61f55323d292f29a9c
joel_sousa <jooelsousa9@gmail.com>
2026-01-11 19:54:42 -0400

    Debug do JavaScript

--- Tela1/script.js
+++ Tela1/script.js
@@ -1,3 +1,21 @@
+//acesso = usuario.isAdmin ? "Administrador" : "usuario";
+
+//console.log(acesso);
+
+//let colaboradorId = ""; 
+
+function login() {
+  const id = document.getElementById("colaboradorId").value;
+
 acesso = usuario.isAdmin ? "Administrador" : "usuario";
 
 console.log(acesso);
+
+{
+let colaboradorId = ""; 
+  }
+
+  {
+    alert("Login realizado: " + id);
+  }
+}
